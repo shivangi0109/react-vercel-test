489e465d8843c8b9bb0a23a764d20e837bbac0cb
Shivangi Srivastava <shi06dec@gmail.com>
2026-08-19 16:45:10 -0600

    Add another paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <>
       <h1>React App</h1>
-      <p>Welcome to your React application!</p>      
+      <p>Welcome to your React application!</p> 
+      <p>Added one more line</p>     
     </>
   )
 }
